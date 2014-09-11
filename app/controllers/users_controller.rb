7e8e13f3d1ad9684da4a67a3a1b052ab47cf24b9
Anna Valkov & lee kebler <student@epicodus.com>
2014-09-11 11:39:10 -0700

    send an email to users when they signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      UserMailer.signup_confirmation(@user).deliver
       session[:user_id] = @user.id
       redirect_to root_path
     else
