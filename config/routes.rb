471c726b602e38c9c071a0a359957c0637fd3c4f
Anna Valkov & lee kebler <student@epicodus.com>
2014-09-11 10:54:19 -0700

    add ability to log in as a user and create a current session.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
  root :to => 'users#index'
  resources :users
+ resources :sessions
 
 end
