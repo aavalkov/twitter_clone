3f19c94e46a1f888779e5b7c396f4039e7b003ae
Anna Valkov & lee kebler <student@epicodus.com>
2014-09-12 12:18:48 -0700

    show users all tweets from their followers

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,4 @@
 class Follow < ActiveRecord::Base
   belongs_to :user
+  belongs_to :followed_user, :class_name => 'User'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
   has_many :follows
+  has_many :followed_users, :through => :follows
+  has_many :f_tweets, :through => :followed_users, :source => :tweets
 
   def followers
     array = []
@@ -13,7 +15,29 @@ class User < ActiveRecord::Base
         array << follow
       end
     end
-      array.count
+    array.count
   end
+
+  def all_followers_tweets
+    tweets = []
+    self.follows.each do |person|
+      user = User.find(person.followed_user_id)
+      user.tweets.each do |tweet|
+        tweets << tweet
+      end
+    end
+    tweets
+  end
+
+
+
+    # <% current_user.follows.each do |tweet| %>
+
+    # <%followed_id = tweet.followed_user_id%>
+    # <% %>
+
+
+    # <% tweeter = '@' + User.find(tweet.user_id).username.capitalize%>
+    # <%=link_to tweeter, user_path(tweet.user_id)%>
 
 end
