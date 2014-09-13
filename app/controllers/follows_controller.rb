c47c3240d013f2824f5d97e72849cbc894f69703
Anna Valkov <anna.a.valkov@gmail.com>
2014-09-13 15:50:19 -0700

    edit layout and rendering

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -8,7 +8,6 @@ class FollowsController < ApplicationController
     Follow.create({user_id: current_user.id, followed_user_id: params[:user_id]})
     follower = User.find(params[:user_id])
     UserMailer.following(current_user, follower).deliver
-    redirect_to root_path
+    redirect_to user_path(follower.id)
   end
 end
-
